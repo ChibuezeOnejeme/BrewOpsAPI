307bfa363be97985b497c4bde3eaf4f36ab64f66
ezemiller <onejemechibueze@gmail.com>
2025-06-30 00:42:16 +0100

    update

--- src/settings.py
+++ src/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 DEBUG =os.getenv("DEBUG")
 SECRET_KEY = os.getenv("SECRET_KEY")
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS")
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
