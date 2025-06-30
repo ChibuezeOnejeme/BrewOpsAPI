8aa37be49631498cf2186281c20d855c43929501
ezemiller <onejemechibueze@gmail.com>
2025-06-30 01:09:56 +0100

    updated

--- src/settings.py
+++ src/settings.py
@@ -14,7 +14,10 @@ from pathlib import Path
 
 from datetime import timedelta
 import os
-import dj_database_url
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -158,8 +161,12 @@ WSGI_APPLICATION = "src.wsgi.application"
 
 # Database configuration from env
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    'default': env.db('DATABASE_URL')
+
 }
+
+
+
 AUTH_USER_MODEL = "accounts.User"
 
 # Password validation
